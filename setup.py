94a53415c5898c83861ee477713fd8b2064c294c
Rashi <rashi@12.com>
2021-12-17 00:10:27 +0530

    files updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME="rasshiagarwala33"
 
 setuptools.setup(
     name="src",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="agarwalrashi543212gmail.com",
     description="Its an modular implemetation of multilevel perceptron",
@@ -23,6 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    packages=["src"],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
