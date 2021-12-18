317929579adf62af498ecaeda06080729eaa5b5a
Rashi <rashi@12.com>
2021-12-18 14:40:51 +0530

    save model added

--- src/training.py
+++ src/training.py
@@ -1,7 +1,8 @@
 from src.utils.common import read_config
 from src.utils.data_mngnt import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model,save_model
 import argparse
+import os 
 
 
 def training(config_path):
@@ -12,10 +13,16 @@ def training(config_path):
     OPTIMIZER=config["params"]["optimizer"]
     METRICS=config["params"]["metrics"]
     NUM_CLASSES=config["params"]["num_class"]
+    model_name=config["artifacts"]["model_name"]
+    artifacts_dir=config["artifacts"]["artifacts_dir"]
+    model_dir=config["artifacts"]["model_dir"]
+    model_path_dir=os.path.join( artifacts_dir,model_dir)
+    os.makedirs(model_path_dir,exist_ok=True)
     EPOCHS=config["params"]["epochs"]
     VALIDATION_SET=(X_valid,y_valid)
     model=create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES)
     history=model.fit(X_train,y_train,epochs=EPOCHS,validation_data=VALIDATION_SET)
+    save_model(model,model_name,model_path_dir)
 
 if __name__ == '__main__':
     args=argparse.ArgumentParser()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,4 +1,8 @@
 import tensorflow as tf 
+import time 
+import os 
+
+
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
     LAYERS=[
@@ -11,3 +15,13 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
     model_clf.summary()
     model_clf.compile(loss=LOSS_FUNCTION,optimizer=OPTIMIZER,metrics=METRICS)
     return model_clf
+
+def get_unique_filename(filename):
+    unique_filename=time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    # .h5 is the format used by keras to store a file 
+    return unique_filename
+
+def save_model(model,model_name,model_dir):
+    unique_filename=get_unique_filename(model_name)
+    path_to_model=os.path.join(model_dir,unique_filename)
+    model.save(path_to_model)
